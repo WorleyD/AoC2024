bdc720ef41439279c25f4db15e0a38d4f1d266bc
worleyd <worleyd@uwindsor.ca>
2024-12-04 18:55:12 -0500

    Removed unused func from day4 part2

--- day4/2.py
+++ day4/2.py
@@ -1,33 +1,11 @@
 grid = [line.strip() for line in open("4.in").readlines()]
 tot=0
-
-def inRange(i,j,r,c):
-	# represents checking upleft, upright, downright, downleft diags
-	vals = [1,1,1,1]
-	if i < 3:
-		vals[0]=0
-		vals[3]=0
-	if i > r-4:
-		vals[1]=0
-		vals[2]=0
-	if j < 3:
-		vals[0]=0
-		vals[1]=0
-	if j > c-4:
-		vals[2]=0
-		vals[3]=0
-	return vals
-
-
-
 r = len(grid)
 c = len(grid[0])
-
 checks=["MS","SM"]
 # for each diag
 for i in range(1,r-1):
 	for j in range(1,c-1):
-		# 4 diags to check, need to be sure we're only checking valid indices
 		if grid[i][j]=="A":
 			if (grid[i-1][j-1]+grid[i+1][j+1] in checks) and (grid[i-1][j+1]+grid[i+1][j-1] in checks): tot+=1 
 
